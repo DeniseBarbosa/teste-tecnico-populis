75c5558c7a1c34d339ecbe611f21000d7e37bf2d
DeniseBarbosa <deniseluvoir@gmail.com>
2023-08-18 14:52:02 -0300

    mesclar e sem duplicatas

--- exercicio-1.js
+++ exercicio-1.js
@@ -8,6 +8,7 @@ function mesclarESemDuplicatas(array1, array2) {
 const array1 = [1, 2, 3, 4, 5];
 const array2 = [4, 5, 6, 7, 8];
 
+
 const mescladoESemDuplicatas = mesclarESemDuplicatas(array1, array2);
 console.log(mescladoESemDuplicatas); 
 
